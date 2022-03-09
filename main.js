85de1f0827201d46da9f275f3159a0395fde5497
prod.astri <58519454+prod-astri@users.noreply.github.com>
2022-03-09 18:24:51 +0100

    simple css header conditionals

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-import '/style.css'
+let width = document.body.clientWidth;
+
+width < 400 && console.log( document.getElementsByTagName('header')[0].style)
+width < 400 && (document.getElementById('header').style.visibility = 'hidden')
+console.log('blow')
+
+console.log(width)
